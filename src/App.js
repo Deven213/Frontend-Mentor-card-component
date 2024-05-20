1e776347fba9906248f73d1ffd57c2acdc12c12e
deven <devenbhimani123@gmail.com>
2024-05-20 18:18:40 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./output.css";
+import { SendansCartSvg } from "./componets/SendansCartSvg";
+import { LuxuryCartSvg } from "./componets/LuxuryCartSvg";
+import { SUVSCartSvg } from "./componets/SUVSCartSvg";
+
+const Bright_orange = "hsl(31,77%,52%)";
+const Dark_cyan = "hsl(184,100%,22%)";
+const Very_dark_cyan = "hsl(179,100%,13%)";
+
+const data = [
+  {
+    bg: Bright_orange,
+    icon: <SendansCartSvg className="mb-8" />,
+    heading: "Sedans",
+    para: "Choose a sedan for its affordability and excellent fuel economy. Ideal for cruising in the city or on your next road trip.",
+  },
+  {
+    bg: Dark_cyan,
+    icon: <LuxuryCartSvg className="mb-8" />,
+    heading: "Luxury",
+    para: "Cruise in the best car brands without the bloated prices. Enjoy the enhanced comfort of a luxury rental and arrive in style.",
+  },
+  {
+    bg: Very_dark_cyan,
+    icon: <SUVSCartSvg className="mb-8" />,
+    heading: "SUVs",
+    para: "Choose a sedan for its affordability and excellent fuel economy. Ideal for cruising in the city or on your next road trip.",
+  },
+];
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div className="min-h-screen w-full flex items-center justify-center p-10 text-white">
+      {data.map((item, index) => (
+        <section
+          key={index}
+          style={{ background: item.bg }}
+          className="w-[260px] py-8 px-10 mx-2 "
         >
-          Learn React
-        </a>
-      </header>
+          {item.icon}
+          <h2 className="uppercase font-bold text-3xl mb-8">{item.heading}</h2>
+          <p className="text-sm leading-relaxed mb-12 font-sans">{item.para}</p>
+          <button
+            style={{ color: item.bg }}
+            className="px-6 bg-white rounded-full hover:ring-1 hover:!text-white hover:bg-inherit py-2 hover:ring-white transition-all"
+          >
+            Learn More
+          </button>
+        </section>
+      ))}
     </div>
   );
 }
---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        "big-shoulders-display": ['"Big Shoulders Display"', "sans-serif"],
+      },
+    },
+  },
+  plugins: [],
+};
